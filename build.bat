b88c7507aaea63c2d550e84c34bd916c66038488
zs-yg <3972006562@qq.com>
2025-11-21 19:12:18 +0800

    添加7z一键压缩至构建脚本

--- build.bat
+++ build.bat
@@ -5,5 +5,9 @@ ren win-x64 MCSJ-x64
 ren win-x86 MCSJ-x86
 rmdir /s /q MCSJ-x64\publish
 rmdir /s /q MCSJ-x86\publish
+7z a -t7z MCSJ-x64.7z MCSJ-x64
+7z a -t7z MCSJ-x86.7z MCSJ-x86
 7z a -tzip MCSJ-x64.zip MCSJ-x64
 7z a -tzip MCSJ-x86.zip MCSJ-x86
+7z a -ttar MCSJ-x64.tar MCSJ-x64
+7z a -ttar MCSJ-x86.tar MCSJ-x86
